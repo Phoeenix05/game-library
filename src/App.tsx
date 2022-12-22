52f696046ba0a4d700f8b3ca9e6aba5641d13ec3
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2022-12-22 17:34:15 +0200

    Added `@solidjs/router`

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import type { Component } from 'solid-js';
+import { Route, Routes } from '@solidjs/router'
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import Home from './routes/Home'
+import Search from './routes/Search'
 
-const App: Component = () => {
+export default function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
-
-export default App;
+    <Routes>
+      <Route path="/" component={Home} />
+      <Route path="/search" component={Search} />
+    </Routes>
+  )
+}
---
+++ src/routes/Search.tsx
@@ -0,0 +1,9 @@
+
+export default function Search() {
+  return (
+    <>
+      Search page 
+      <a href="/">home</a>
+    </>
+  )
+}
